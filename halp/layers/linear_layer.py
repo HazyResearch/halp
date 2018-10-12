c66c8084a433330d8b998c76e47105b97256c425
zjian <zjian@stanford.edu>
2018-10-11 23:38:32 -0700

    switch to develop on gpu

--- halp/layers/linear_layer.py
+++ halp/layers/linear_layer.py
@@ -19,11 +19,11 @@ class BitCenterLinear(nn.Linear):
         # TODO make weight_lp bias_lp all in no-gradient mode
         # TODO check if weight delta is with gradient
         self.weight_delta = self.cast_func(self.weight)
-        self.weight_lp = Parameter(self.cast_func(self.weight.data), require_grad=False)
+        self.weight_lp = Variable(self.cast_func(self.weight.data), requires_grad=False)
         self.do_offset = False
         if bias:
             self.bias_delta = self.cast_func(self.bias)
-            self.bias_lp = Parameter(self.cast_func(self.bias.data), require_grad=False)
+            self.bias_lp = Variable(self.cast_func(self.bias.data), requires_grad=False)
         else:
             self.register_parameter('bias_delta', None)
             self.register_parameter('bias_lp', None)
@@ -31,9 +31,12 @@ class BitCenterLinear(nn.Linear):
         self.reset_parameters_bit_center()
 
     def reset_parameters_bit_center(self):
-        init.zero_(self.weight_delta)
+        # init.zeros_(self.weight_delta)
+        # self.weight_delta = self.weight_delta.fill_(0.0)
+        self.weight_delta = self.cast_func(torch.zeros_like(self.weight_delta.float()))
         if self.bias is not None:
-            init.zero_(self.bias_delta)
+            self.bias_delta = self.cast_func(torch.zeros_like(self.bias_delta.float()))
+            # init.zeros_(self.bias_delta)
 
     def set_mode(do_offset):
         self.do_offset = offset
@@ -49,47 +52,7 @@ class BitCenterLinear(nn.Linear):
             return F.linear(input, self.weight, self.bias)
         else:
             # For here input is delta_x
-            return self.weight_lp * input + (self.input_lp + input) * self.weight_delta
-
-
-# def GetToyBitCenterLogistic():
-#     n_sample = 4
-#     n_dim = 3
-#     n_class = 4
-#     X = Variable(torch.DoubleTensor(np.random.normal(size=(n_sample, n_dim) ) ) )
-#     Y = Variable(torch.LongTensor(np.array([0, 1, 3, 2] ) ) )
-#     regressor = LogisticRegression(input_dim=n_dim, n_class=n_class, reg_lambda=100.0, dtype="half")
-#     # loss1 = regressor.forward(X, Y)
-#     # loss_diff = 0.0
-#     # move = 1e-9
-#     # loss1.backward()
-#     # for w in regressor.parameters():
-#     # loss_diff += torch.sum(w.grad.data * move)
-#     # for w in regressor.parameters():
-#     # w.data += move
-#     # loss2 = regressor.forward(X, Y)
-#     # # assert np.abs((loss2[0] - loss1[0] ).data.cpu().numpy() - loss_diff) < 1e-9
-#     # # print("loss finite diff ", loss2[0] - loss1[0], " projected loss change ", loss_diff)
-#     # print("logistic regression gradient test done!")
-#     return X, Y, regressor
-
-
-# def RunToyBitCenterLogistic():
-#     X, Y, regressor = GetToyBitCenterLogistic()
-#     loss1 = regressor.forward(X, Y)
-#     loss_diff = 0.0
-#     move = 1e-9
-#     loss1.backward()
-#     for w in regressor.parameters():
-#         loss_diff += torch.sum(w.grad.data * move)
-#     for w in regressor.parameters():
-#         w.data += move
-#     loss2 = regressor.forward(X, Y)
-#     # assert np.abs((loss2[0] - loss1[0] ).data.cpu().numpy() - loss_diff) < 1e-9
-#     # print("loss finite diff ", loss2[0] - loss1[0], " projected loss change ", loss_diff)
-#     print("logistic regression gradient test done!")
-
-
-# if __name__ == "__main__":
-#     RunToyBitCenterLogistic()
-        
+            # print("inside ", self.weight_lp.dtype, input.dtype)
+            # self.weight_lp.add_(self.weight_lp)
+            # torch.matmul(self.weight_lp, input)
+            return torch.mm(self.weight_lp, input) + torch.mm(self.weight_delta, (self.input_lp + input))
