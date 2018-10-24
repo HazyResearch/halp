44c7a73b0e97d8b45303274e8197921c8ab638dd
zjian <zjian@stanford.edu>
2018-10-24 16:35:49 -0700

    refactor done to be tested on layer and test

---
+++ halp/layers/bit_center_layer.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from torch.nn.parameter import Parameter
+from torch.autograd import Function
+from torch.autograd import Variable
+import numpy as np
+from halp.utils.utils import void_cast_func, single_to_half_det, single_to_half_stoc, void_func
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+class BitCenterLayer(nn.Module):
+    def __init__(self, fp_functional=void_func, lp_functional=void_func, bias=True, 
+        cast_func=void_cast_func, n_train_sample=1):
+        nn.Module.__init__(self)
+        self.cast_func = cast_func
+        # register the fp and lp forward function
+        self.fp_func = fp_functional
+        self.lp_func = lp_functional
+        # input cache
+        self.input_cache = None
+        self.grad_output_cache = None
+        self.cache_iter = 0
+        self.grad_cache_iter = 0
+        self.n_train_sample = n_train_sample
+        # starting from fp mode
+        self.set_mode(do_offset=True)
+
+    def setup_bit_center_vars(self):
+        self.weight_delta = Parameter(self.cast_func(self.weight.data), requires_grad=True)
+        self.weight_lp = Parameter(self.cast_func(self.weight.data), requires_grad=False)
+        self.set_mode(do_offset=True)
+        if self.bias is not None:
+            self.bias_delta = Parameter(self.cast_func(self.bias), requires_grad=True)
+            self.bias_lp = Parameter(self.cast_func(self.bias.data), requires_grad=False)
+        else:
+            self.register_parameter('bias_delta', None)
+            self.register_parameter('bias_lp', None)
+
+    def reset_parameters_bit_center(self):
+        init.zeros_(self.weight_delta)
+        if self.bias is not None:
+            init.zeros_(self.bias_delta)
+
+    def set_mode(self, do_offset, cache_iter=0):
+        self.do_offset = do_offset
+        self.cache_iter = cache_iter
+
+    def setup_cache(self, input):
+        # the cache is set up when the first minibatch forward is done.
+        # here we assume the first dimension of input blob indicates the size of minibatch
+        cache_shape = list(input.size())
+        cache_shape[0] = self.n_train_sample
+        cache = self.cast_func(Variable(torch.zeros(cache_shape).type(input.dtype))).cpu()
+        return cache
+
+    def update_grad_output_cache(self, self1, input, output):
+        pass
+
+    def update_input_cache(self, input):
+        if self.do_offset:
+            self.input_cache[self.cache_iter:min(self.cache_iter + input.size()[0], 
+                self.n_train_sample)].data.copy_(self.cast_func(input.cpu()))
+            self.cache_iter = (self.cache_iter + input.size(0)) % self.n_train_sample
+
+    def forward_fp(self, input):
+        if self.input_cache is None:
+            self.input_cache = self.setup_cache(input)
+            self.cache_iter = 0
+        output = self.fp_func(input, self.weight, self.bias)
+        if self.grad_output_cache is None:
+            self.grad_output_cache = self.setup_cache(output)
+            self.grad_cache_iter = 0
+        self.update_input_cache(input)
+        return output
+
+    def forward_lp(self, input):
+        # Need to test do_offset mode whether gradient is updated properly
+        input_lp = self.input_cache[self.cache_iter:(self.cache_iter + input.size(0))].cuda()
+        grad_output_lp = \
+            self.grad_output_cache[self.grad_cache_iter:(self.grad_cache_iter + input.size(0))].cuda()
+        input_delta = input
+        weight_lp = self.weight_lp
+        weight_delta = self.weight_delta
+        bias_lp = self.bias_lp
+        bias_delta = self.bias_delta
+        output = self.lp_func(input_delta, input_lp, grad_output_lp,
+            weight_delta, weight_lp, bias_delta, bias_lp)
+        self.cache_iter = (self.cache_iter + input.size(0)) % self.n_train_sample
+        self.grad_cache_iter = (self.grad_cache_iter + input.size(0)) % self.n_train_sample
+        return output
+
+    def forward(self, input):
+        # Need to test do_offset mode whether gradient is updated properly
+        if self.do_offset:
+            return self.forward_fp(input)
+        else:
+            return self.forward_lp(input)
+
---
+++ halp/layers/bit_center_layer_test.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+from halp.layers.linear_layer import BitCenterLinear, bit_center_linear
+from torch.autograd import gradcheck
+from halp.utils.utils import void_cast_func, single_to_half_det, single_to_half_stoc
+from unittest import TestCase
+from halp.utils.test_utils import HalpTest
+from torch.autograd.gradcheck import get_numerical_jacobian, iter_tensors, make_jacobian
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+class TestBitCenterLayer(HalpTest):
+    '''
+    Test the functionality of bit centering linear layers
+    the helper functions implemented here should directy serve for linear style
+    layers such as linear layer conv layer and etc.
+    '''
+    def prepare_layer(self, minibatch_size, dim_in, dim_out, bias):
+        """
+        this function should generate the layer to be tested and the fp lp input
+        """
+        pass
+
+    def get_analytical_grad(self, layer):
+        layer.set_mode(do_offset=True)
+        output_fp = layer(input_fp)
+        output_fp_copy = output_fp.data.clone()
+        loss_fp = torch.sum(0.5*output_fp*output_fp)
+        loss_fp.backward()
+        grad_input_fp = layer.input_grad_for_test.clone()
+
+        layer.set_mode(do_offset=False)
+        output_lp = layer(input_delta)
+        loss_lp = torch.sum(0.5*output_lp*output_lp)
+        loss_lp.backward()
+        grad_input_delta = layer.input_grad_for_test.clone()
+
+        weight_grad = layer.weight.grad + layer.weight_delta.grad
+        if layer.bias is not None:
+            bias_grad = layer.bias.grad + layer.bias_delta.grad
+        # as we only have 1 minibatch, we can directly use layer.grad_output_cache
+        input_grad = grad_input_fp + grad_input_delta
+        return output_lp + output_fp, [input_grad, weight_grad, bias_grad]
+
+    def get_numerical_grad(self, layer):
+        # get numerical finite difference
+        layer.set_mode(do_offset=True)
+        def get_loss(x):
+            output = layer(x)
+            return torch.sum(0.5*output*output)
+        grad_list = []
+        layer.set_mode(do_offset=True)
+        layer.weight.data.add_(layer.weight_delta.data)
+        if layer.bias is not None:
+            layer.bias.data.add_(layer.bias_delta.data)
+        output_final = layer(input_fp + input_delta)
+        num_input_grad = get_numerical_jacobian(get_loss, input_fp + input_delta, 
+            target=None, eps=perturb_eps)
+        grad_list.append(num_input_grad)
+        num_weight_grad = get_numerical_jacobian(get_loss, input_fp + input_delta, 
+            target=layer.weight, eps=perturb_eps)
+        grad_list.append(num_input_grad)
+        if layer.bias is not None:
+            num_bias_grad = get_numerical_jacobian(get_loss, input_fp + input_delta, 
+                target=layer.bias, eps=perturb_eps)
+            grad_list.append(num_bias_grad)
+        return output, grad_list
+
+    def test_forward_backward_output(self):
+        # test if backward is synced with forward in double mode
+        # test on a single batch of data, check if the 
+        # gradient can give similar numerical loss changes
+        # In this test, we use the quadratic loss, this is because
+        # using this loss, the grad_output decompose directly to offset and delta
+        # in the fp and lp steps
+        minibatch_size = 35
+        dim_in = 24
+        dim_out = 17
+        perturb_eps = 1e-6
+        rtol_num_analytical_grad = 5e-4
+        np.random.seed(0)
+        torch.manual_seed(0)
+        torch.cuda.manual_seed_all(0)
+        for bias in [True, False]:
+            for i in range(10):
+                layer, input_fp, input_delta = self.prepare_layer(minibatch_size, dim_in, dim_out, bias)
+                analytical_output, analytical_grads = self.get_analytical_grad(layer)
+                numerical_output, numerical_grads = self.get_numerical_grad(layer)
+                assert len(analytical_grads) == len(numerical_grads)
+
+                np.testing.assert_allclose(analytical_output.data.cpu().numpy().ravel(), 
+                    numerical_output.data.cpu().numpy().ravel(), 
+                    rtol=rtol_num_analytical_grad)
+                for ana_grad, num_grad in zip(analytical_grads, numerical_grads):
+                    np.testing.assert_allclose(analytical_grads.data.cpu().numpy().ravel(), 
+                        numerical_grads.data.cpu().numpy().ravel(), 
+                        rtol=rtol_num_analytical_grad)
+        logger.info("Bit centering linear function test passed!")
+
+    def check_layer_param_and_cache(self, layer):
+        t_list = [(layer.weight, torch.float32, True, True), 
+                  (layer.bias, torch.float32, True, True),
+                  (layer.weight_delta, torch.half, True, True),
+                  (layer.bias_delta, torch.half, True, True),
+                  (layer.weight_lp, torch.half, True, False),
+                  (layer.bias_lp, torch.half, True, False),
+                  (layer.input_cache, torch.half, False, False)]
+        self.CheckLayerTensorProperty(t_list)
+        self.CheckLayerTensorGradProperty(t_list)
+
+    def test_layer_forward_backward_precedures(self, cast_func=single_to_half_det):
+        # We test the behavior of layers in a multiple epoch (each with multiple minibatch setting).
+        # Along with this, we will also test the property of tensors 
+        # (including param and cache along the way).
+        # check if the behavior of BitCentering linear layer is going as expected for forward
+        # the backward behavior is guaranteed by 
+        # bit center linear function test TestBitCenterLinearFuncGradientCheck
+        n_sample = 98
+        n_dim = 13
+        n_out_dim = 31
+        minibatch_size = 33
+        n_minibatch = int(np.ceil(n_sample / minibatch_size))
+        for use_bias in [True, False]:
+            # use_bias = True
+            layer, _, _ = self.prepare_layer(minibatch_size, dim_in, dim_out, bias)
+
+            # layer = BitCenterLinear(in_features=n_dim, out_features=n_out_dim, 
+            #     bias=use_bias, cast_func=cast_func, n_train_sample=n_sample)
+            # test fp mode
+            layer.set_mode(do_offset=True)
+            layer.cuda()
+            input_tensor_list = []
+            if (cast_func == single_to_half_det) or (cast_func == single_to_half_stoc): 
+                self.check_layer_param_and_cache(layer)
+            for i in range(n_minibatch):
+                start_idx = i * minibatch_size
+                end_idx = min((i + 1) * minibatch_size, n_sample)
+                if i !=0:
+                    input_cache_before = layer.input_cache[start_idx:end_idx].clone().numpy()
+                    grad_input_cache_before = layer.grad_output_cache[start_idx:end_idx].clone().numpy()
+                input_tensor = torch.randn(end_idx - start_idx, n_dim, dtype=torch.float32, requires_grad=True).cuda()        
+                output = layer(input_tensor)
+                input_tensor_list.append(input_tensor)
+                torch.sum(output).backward()
+                if i != 0:
+                    input_cache_after = layer.input_cache[start_idx:end_idx, :].numpy()
+                    grad_input_cache_after = layer.grad_output_cache[start_idx:end_idx, :].numpy()
+                    assert (input_cache_before == 0).all()
+                    assert (grad_input_cache_before == 0).all()
+                    assert (input_cache_before != input_cache_after).all()
+                    assert (grad_input_cache_before != grad_input_cache_after).all()
+            # test lp mode
+            layer.set_mode(do_offset=False)
+            if (cast_func == single_to_half_det) or (cast_func == single_to_half_stoc): 
+                self.check_layer_param_and_cache(layer)
+            for i in range(n_minibatch):
+                input_lp = layer.input_cache[layer.cache_iter:(layer.cache_iter + input_tensor_list[i].size(0))].cuda()
+                output = layer(cast_func(input_tensor_list[i]))
+                torch.sum(output).backward()
+            if (cast_func == single_to_half_det) or (cast_func == single_to_half_stoc): 
+                self.check_layer_param_and_cache(layer)
+        print("Bit centering linear layer test passed!")
+
+
+# if __name__ == "__main__":
+#     print(torch.__version__)
+#     unittest.mian()
