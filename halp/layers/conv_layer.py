ac956a6494ab1823ea36e01c588cac592eef9b57
zjian <zjian@stanford.edu>
2018-11-06 15:37:19 -0800

    conv test temporarily passed

--- halp/layers/conv_layer.py
+++ halp/layers/conv_layer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from math import floor
 from torch.nn import Conv2d
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
@@ -16,7 +17,30 @@ logger = logging.getLogger()
 
 class BitCenterConv2DFunction(Function):
     """
-    TODO: DOCUMENT the mathmatical equations for the gradient computation
+    This class does forward and backward computation for bit center 
+    2D convolution. We document the forward backward computation math
+    here for normal conv2d. The math for bit center Conv2d can be derived
+    by decomposing the input and weights into full precision offset and 
+    low precision delta.
+    Given input X in the shape of (b, n_i, w_i, h_i)
+    and filter W in the shape of (n_o, n_i, w_k, h_k)
+    For simplicity we write out the math with 0 padding and stride 1 
+    in the following:
+    we transform the input tensor X to a matrix X_L in the shape of 
+    (b, w_o * h_o, n_i * w_k * h_k). W is transformed into W_L in the shape of
+    (n_i * w_k * h_k, n_o).
+    In the forward pass,
+    \tilde{X} = matmul(X_L, W_L), where \tilde{X} is in the shape of
+    (b, w_o * h_o, n_o)
+    In the backward pass,
+    \par L / \par W_L = matmul(X_L^T, \par L / \par \tilde{X}_L)
+    \par L / \par X_L = matmul(\par L / \par \tilde{X}_L, W_L^T)
+    Note here \par L / \par X_L can be directly done using deconv operations
+    We opt to use fold and unfold function to explicitly do 
+    \par L / \par W_L = matmul(X_L^T, \par L / \par \tilde{X}_L)
+    because we the current deconv (conv_transpose2d) API has some flexibility issue.
+    Note \par L / \par W_L, and \par L / \par X_L are in the shape of 
+    () and () respectively. We need properly reshape to return the gradient
     """
     @staticmethod
     def forward(ctx,
@@ -35,48 +59,67 @@ class BitCenterConv2DFunction(Function):
         # suffix delta means the real low precision part of the model representation
         # output_grad_lp is only for backward function, but we need to keep it in ctx
         # for backward function to access it.
-        kernel_size = weight.size()[-2:]
+        # TODO: extend to accommodate different stride, padding, dilation, groups
+        assert (stride, padding, dilation, groups) == (1, 0, 1, 1) 
+        batch_size = input_lp.size(0)
+        kernel_size = np.array(weight_lp.size()[-2:]).astype(np.int)
+        input_size = np.array(input_lp.size()[-2:]).astype(np.int)
+        output_size = np.floor((input_size + 2 * padding - dilation *
+                                (kernel_size - 1) - 1) / stride + 1).astype(np.int)
+        kernel_size = kernel_size.tolist()
+        input_size = input_size.tolist()
+        output_size = output_size.tolist()
         input_lp_unf = F.unfold(input_lp, kernel_size)
         input_delta_unf = F.unfold(input_delta, kernel_size)
-        ctx.save_for_backward(input_lp_unf, input_delta_unf, output_grad_lp, weight_lp,
-                              weight_delta, bias_lp, bias_delta)
+        ctx.save_for_backward(input_lp_unf, input_delta_unf, output_grad_lp,
+                              weight_lp, weight_delta, bias_lp, bias_delta)
         ctx.hyperparam = (stride, padding, dilation, groups)
-        #\tilda X = mm(X_L, W_L) 
+        #\tilda X = mm(X_L, W_L)
         conv2d = lambda input_unf, weight: \
-            torch.mm(input_unf.transpose(1, 2), 
+            input_unf.transpose(1, 2).matmul(
             weight.permute(1, 2, 3, 0).view(-1, weight.size(0)))
         output = conv2d(input_delta_unf, weight_lp) \
             + conv2d(input_lp_unf + input_delta_unf, weight_delta)
-        input_size = np.array(input_lp_unf.size()[-2:]).astype(np.int)
-        output_size = np.floor((input_size + 2 * padding - dilation * (kernel_size - 1) - 1) / stride + 1)
-        output = F.fold(output, output_size=output_size, kernel_size=kernel_size)
+        # print(output.size(), output_size, kernel_size, input_lp.size(), weight_lp.size())
+        channel_out = weight_lp.size(0)
+        output = output.transpose(1, 2).view(batch_size, channel_out, *output_size)
+        if bias_delta is not None:
+            output = output + bias_delta.view(1, -1, 1, 1).expand_as(output)
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
         '''
-    	Given O = X * F, we have
-    	\par L / \par F = X * \par L / \par O
-    	\par L / \par X = \par L / \par O * flip_180(w)
-    	The shape of the tensor: N, C_{in}, H_{in}, W_{in}
+        In this function, suffix represent the results from torch unfold style im2col op
     	'''
-        # TODO assert in both forward and backward, checking whether the condition is 
-        # satisfied
-        input_lp, input_delta, output_grad_lp, \
+        # TODO extend to accommodate more configs for stride, padding, dilation, groups
+        input_lp_unf, input_delta_unf, output_grad_lp, \
             weight_lp, weight_delta, bias_lp, bias_delta = ctx.saved_tensors
         stride, padding, dilation, groups = ctx.hyperparam
-        deconv2d = lambda input, weight, bias: F.conv_trasnpose2d(input, weight, bias, stride, padding, dilation, groups)
+        assert (stride, padding, dilation, groups) == (1, 0, 1, 1) 
+        batch_size, channel_out, w_out, h_out = list(grad_output.size())
+        channel_in = weight_lp.size(1)
+        kernel_size = weight_lp.size()[-2:] 
+        assert channel_out == weight_lp.size(0)
+        # reshape output grad for further computation
+        grad_output_reshape = grad_output.permute(0, 2, 3, 1).view(batch_size, -1, channel_out)
+        output_grad_lp_reshape = output_grad_lp.permute(0, 2, 3, 1).view(batch_size, -1, channel_out)
+        # define function to get gradient wrt input and weights
+        get_grad_input = lambda grad_out_reshape, weight: \
+            F.conv_transpose2d(grad_out_reshape, weight, bias=None)
+        get_grad_weight = lambda input_unf, grad_output_reshape: \
+            torch.sum(torch.bmm(input_unf, grad_output_reshape), dim=0)
         grad_input_lp = None
-
-        # print("check in backward ", grad_output.shape, weight_lp.shape, torch.flip((weight_lp + weight_delta), [2, 3]).shape)
-
         grad_input_delta = \
-         deconv2d(grad_output, (weight_lp + weight_delta), None) \
-         + deconv2d(output_grad_lp, weight_delta, None)
+            get_grad_input(grad_output, (weight_lp + weight_delta)) \
+            + get_grad_input(output_grad_lp, weight_delta)
         grad_output_grad_lp = None  # this dummy to adapt to pytorch API
         grad_weight_lp = None
-        grad_weight_delta = deconv2d(grad_output, input_lp + input_delta, None)[:, :, :, :] \
-         + conv2d(output_grad_lp, input_delta, None)[:, :, :, :]
+        grad_weight_delta = \
+            get_grad_weight(input_lp_unf + input_delta_unf, grad_output_reshape) \
+            + get_grad_weight(input_delta_unf, output_grad_lp_reshape)
+        grad_weight_delta = \
+            grad_weight_delta.view(channel_in, *kernel_size, channel_out).permute(3, 0, 1, 2)
         grad_bias_lp = None
         if (bias_lp is not None) and (bias_delta is not None):
             grad_bias_delta = grad_output.sum(dim=[0, 2, 3])
@@ -129,7 +172,10 @@ class BitCenterConv2D(BitCenterLayer, Conv2d):
 
     def update_grad_output_cache(self, self1, input, output):
         # use duplicated self to adapt to the pytorch API requirement
-        # as this is a class member function
+        # as this is a class member function.
+        # TODO: consider merge this function into the base BitCenterLayer
+        # class. Currently it is not doable because of the behavior described
+        # in BitCenterLinear layer.
         if self.do_offset:
             self.grad_output_cache[self.grad_cache_iter:min(
                 self.grad_cache_iter +
@@ -137,16 +183,5 @@ class BitCenterConv2D(BitCenterLayer, Conv2d):
                     self.cast_func(output[0].cpu()))
             self.grad_cache_iter = (
                 self.grad_cache_iter + output[0].size(0)) % self.n_train_sample
-            # we use the following variable only for test purpose, we want to be able to access
-            # the gradeint value wrt input in the outside world. For lp mode, it is grad_input_delta
-            # for fp mode, it is grad_input
-            # TODO: update if pytorch stable version fixes this:
-            # The following branch is due to the layer specific behavior of
-            # input argument to the backward hook.
-            # Here we hard code the order of tensor in the input list (this is layer specific)
-            if self.bias is not None:
-                self.input_grad_for_test = input[1]
-            else:
-                self.input_grad_for_test = input[0]
-        else:
-            self.input_grad_for_test = input[0]
+        self.input_grad_for_test = input[0]
+
--- halp/layers/conv_layer_test.py
+++ halp/layers/conv_layer_test.py
@@ -50,10 +50,10 @@ class TestBitCenterConv2DLayer(TestBitCenterLayer, TestCase):
             cast_func=cast_func,
             n_train_sample=n_train_sample)
 
-        print("ckpt 1 ", layer.weight.shape, layer.bias.shape, \
-            torch.randn(
-                    dim_out, dim_in, *layer.kernel_size, dtype=torch.double,
-                    requires_grad=False).cuda().shape)
+        # print("ckpt 1 ", layer.weight.shape, layer.bias.shape, \
+        #     torch.randn(
+        #             dim_out, dim_in, *layer.kernel_size, dtype=torch.double,
+        #             requires_grad=False).cuda().shape)
 
 
         # Note do_double = setup layer for gradient check, otherwise, it is for checking
@@ -114,70 +114,83 @@ class TestBitCenterConv2DLayer(TestBitCenterLayer, TestCase):
             input_fp = Parameter(
                 torch.randn(n_train_sample, dim_in, INPUT_W, INPUT_H, dtype=torch.float).cuda(),
                 requires_grad=True)
-
-        print("hyperparam ", dim_in, dim_out, input_delta.shape, input_fp.shape)
-
         return [input_fp,], [input_delta,]
 
 
 
-def init_test():
-    set_seed(0)
-    CHANNEL_IN=5
-    CHANNEL_OUT=6
-    BATCH_SIZE=10
-    INPUT_W=4
-    INPUT_H=4
-    KERNEL_SIZE=(3, 3)
-    # INPUT_W=1
-    # INPUT_H=1
-    # KERNEL_SIZE=(1, 1)
-    STRIDE=1
-    PADDING=0
+# def init_test():
+#     set_seed(0)
+#     CHANNEL_IN=5
+#     CHANNEL_OUT=6
+#     BATCH_SIZE=10
+#     INPUT_W=4
+#     INPUT_H=4
+#     KERNEL_SIZE=(3, 3)
+#     # INPUT_W=1
+#     # INPUT_H=1
+#     # KERNEL_SIZE=(1, 1)
+#     STRIDE=1
+#     PADDING=0
 
-    input_val = Parameter(torch.randn(BATCH_SIZE, CHANNEL_IN, INPUT_W, INPUT_H), requires_grad=True)
-    layer = torch.nn.Conv2d(CHANNEL_IN, CHANNEL_OUT, KERNEL_SIZE, bias=False)
-    output = layer(input_val)
+#     input_val = Parameter(torch.randn(BATCH_SIZE, CHANNEL_IN, INPUT_W, INPUT_H), requires_grad=True)
+#     layer = torch.nn.Conv2d(CHANNEL_IN, CHANNEL_OUT, KERNEL_SIZE, bias=False)
+#     output = layer(input_val)
 
-    print("input ", input_val.shape, output.shape)
+#     print("input ", input_val.shape, output.shape)
 
-    loss = torch.sum(output)
-    loss.backward()
+#     loss = torch.sum(output)
+#     loss.backward()
 
-    output_grad = torch.ones_like(output)
-    # input_grad = torch.nn.functional.conv_transpose2d(output_grad, layer.weight.data)
-    # weight_grad = torch.nn.functional.conv_transpose2d(output_grad, input_val)
-    # print("pre check ", output_grad.shape, input_val.shape, output_grad.transpose(0, 1).shape)
+#     output_grad = torch.ones_like(output)
+#     # input_grad = torch.nn.functional.conv_transpose2d(output_grad, layer.weight.data)
+#     # weight_grad = torch.nn.functional.conv_transpose2d(output_grad, input_val)
+#     # print("pre check ", output_grad.shape, input_val.shape, output_grad.transpose(0, 1).shape)
 
-    # print("pt grad weight ", layer.weight.grad, layer.weight.grad.shape)
-    # print("dc grad weight ", weight_grad[:, :, 1:-1, 1:-1], weight_grad.shape)
+#     # print("pt grad weight ", layer.weight.grad, layer.weight.grad.shape)
+#     # print("dc grad weight ", weight_grad[:, :, 1:-1, 1:-1], weight_grad.shape)
 
-    # using the im2col col2im approaches
-    output_grad_unf = output_grad.permute(0, 2, 3, 1).view(output_grad.size(0), -1, output_grad.size(1))
-    input_unf = torch.nn.functional.unfold(input_val, KERNEL_SIZE)
+#     # using the im2col col2im approaches
+#     output_grad_unf = output_grad.permute(0, 2, 3, 1).view(output_grad.size(0), -1, output_grad.size(1))
+#     input_unf = torch.nn.functional.unfold(input_val, KERNEL_SIZE)
 
-    # print("test ", output_grad_unf.shape, input_unf.shape)
+#     # print("test ", output_grad_unf.shape, input_unf.shape)
 
 
-    grad_weight = torch.bmm(input_unf, output_grad_unf).sum(dim=0)
-    grad_weight = grad_weight.view(CHANNEL_IN, *KERNEL_SIZE, CHANNEL_OUT).permute(3, 0, 1, 2)
-    weight = layer.weight.view(CHANNEL_OUT, -1)
+#     grad_weight = torch.bmm(input_unf, output_grad_unf).sum(dim=0)
+#     grad_weight = grad_weight.view(CHANNEL_IN, *KERNEL_SIZE, CHANNEL_OUT).permute(3, 0, 1, 2)
+#     weight = layer.weight.view(CHANNEL_OUT, -1)
 
-    # print("test shape ", output_grad_unf.shape, weight.shape)
+#     # print("test shape ", output_grad_unf.shape, weight.shape)
 
-    grad_input = output_grad_unf.matmul(weight)
-    grad_input = torch.nn.functional.fold(grad_input.transpose(1, 2), 
-        output_size=(INPUT_W, INPUT_H), kernel_size=KERNEL_SIZE)
-    print("pt grad weight ", torch.sum(layer.weight.grad**2), layer.weight.grad.shape)
-    print("dc grad weight ", torch.sum(grad_weight**2), grad_weight.shape)
+#     grad_input = output_grad_unf.matmul(weight)
+#     grad_input = torch.nn.functional.fold(grad_input.transpose(1, 2), 
+#         output_size=(INPUT_W, INPUT_H), kernel_size=KERNEL_SIZE)
+#     print("pt grad weight ", torch.sum(layer.weight.grad**2), layer.weight.grad.shape)
+#     print("dc grad weight ", torch.sum(grad_weight**2), grad_weight.shape)
 
-    print("pt grad input ", torch.sum(input_val.grad**2), input_val.grad.shape)
-    print("dc grad input ", torch.sum(grad_input**2), grad_input.shape)
+#     print("pt grad input ", torch.sum(input_val.grad**2), input_val.grad.shape)
+#     print("dc grad input ", torch.sum(grad_input**2), grad_input.shape)
 
 
 if __name__ == "__main__":
-    # print(torch.__version__)
-    # unittest.main()
+    print(torch.__version__)
+    unittest.main()
 
-    init_test()
+    # init_test()
 
+    # unfold = torch.nn.Unfold(kernel_size=(2, 3))
+    # input = torch.randn(2, 5, 3, 4)
+    # output = unfold(input)
+    # output.size()
+    
+    # # Convolution is equivalent with Unfold + Matrix Multiplication + Fold (or view to output shape)
+    # inp = torch.randn(1, 3, 10, 12) 
+    # w = torch.randn(2, 3, 4, 5)
+    # inp_unf = torch.nn.functional.unfold(inp, (4, 5))
+    # out_unf = inp_unf.transpose(1, 2).matmul(w.view(w.size(0), -1).t()).transpose(1, 2)
+    # out_unf[:] = 1.0
+    # out = torch.nn.functional.fold(out_unf, (7, 8), (1, 1))
+
+    # print("check output ", out)
+
+    # print((torch.nn.functional.conv2d(inp, w) - out).abs().max())
