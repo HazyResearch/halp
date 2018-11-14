1cba321e3c2ad0e880157415a12ff1d849eef277
cfncluster user <ubuntu@ip-172-31-35-73.ec2.internal>
2018-11-14 18:40:46 +0000

    first round sweep of log reg and lenet nov-13

--- halp/exp_script/logreg_sweep_nov_13.py
+++ halp/exp_script/logreg_sweep_nov_13.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
     T_list = [600]
     dataset = "mnist"
     model = "logreg"
-    run_option = "dryrun"
-    # run_option = "run"
+    #run_option = "dryrun"
+    run_option = "run"
     run_experiment(
         exp_name,
         n_epochs,
@@ -43,6 +43,6 @@ if __name__ == "__main__":
         T_list,
         dataset,
         model,
-        cluster="dawn",
-        # cluster="starcluster",
+        #cluster="dawn",
+        cluster="starcluster",
         run_option=run_option)
