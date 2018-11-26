90c4fed10d124a89031aa435388ea2fdbed519e0
zjian <zjian@stanford.edu>
2018-11-26 14:27:55 -0800

    pooling layer refactored

--- halp/layers/max_pool_layer.py
+++ halp/layers/max_pool_layer.py
@@ -75,10 +75,91 @@ class BitCenterMaxPool2DFunction(Function):
         return grad_input_delta, grad_input_lp, grad_grad_output_lp, grad_kernel_size, grad_stride, grad_padding
 
 
+class BitCenterAvgPool2DFunction(Function):
+    @staticmethod
+    def forward(ctx,
+                input_delta,
+                input_lp,
+                grad_output_lp,
+                kernel_size,
+                stride=None,
+                padding=0):
+        input_full = input_lp + input_delta
+        output_full = F.avg_pool2d(
+            input_full,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding)
+        # The following line is just for simulation, the output and indices can be cached
+        output_lp = F.avg_pool2d(
+            input_lp,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding)
+        ctx.save_for_backward(grad_output_lp)
+        ctx.hyperparam = (kernel_size, stride, padding, input_full.shape)
+        if type(padding) is list:
+            raise Exception("tuple based padding ")
+        return output_full - output_lp
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # note here pytorch avg pool layer set stride = kernel size if not specified
+        if (stride != kernel_size):
+            raise Exception("stride is only supported when it is equal to kernel size!")
+        kernel_size, stride, padding, input_shape = ctx.hyperparam
+        grad_output_lp = ctx.saved_tensors
+        grad_output_full = grad_output + grad_output_lp
+
+        shape = list(grad_output.shape)
+        grad_input = grad_output.view(*shape, 1).contiguous()\
+            .expand(*shape, kernel_size[1]).contiguous()\
+            .view(*shape[:-1], shape[-1]*kernel_size[-1])
+        shape = list(grad_input.shape)
+        grad_input = grad_input.view(*shape[:-1], 1, shape[-1]).contiguous() \
+            .expand(*shape[:-1], kernel_size[0], shape[-1]).contiguous()\
+            .view(*shape[:-2], shape[-2] * kernel_size[0], shape[-1])
+        if list(grad_input.shape) != list(input_shape):
+            grad_input_tmp = grad_input
+            grad_input = np.zeros(input_shape)
+            grad_input[:, :, :grad_input_tmp.size(2), :grad_input_tmp.size(3)] = grad_input_tmp
+        grad_input_delta = grad_input_full - grad_input_lp
+
+        grad_input_lp = None
+        grad_grad_output_lp = None
+        grad_kernel_size = None
+        grad_stride = None
+        grad_padding = None
+        return grad_input_delta, grad_input_lp, grad_grad_output_lp, grad_kernel_size, grad_stride, grad_padding
+
+
 bit_center_max_pool2d = BitCenterMaxPool2DFunction.apply
 
+bit_center_avg_pool2d = BitCenterAvgPool2DFunction.apply
 
-class BitCenterMaxPool2D(BitCenterLayer, nn.MaxPool2d):
+
+class BitCenterPool2D(BitCenterLayer):
+    def forward_lp(self, input):
+        input_lp, grad_output_lp = self.get_input_cache_grad_cache(input)
+        input_delta = input
+        output = self.lp_func(input_delta, input_lp, grad_output_lp,
+                              self.kernel_size, self.stride, self.padding)
+        self.increment_cache_iter(input)
+        return output
+
+    def forward_fp(self, input):
+        self.check_or_setup_input_cache(input)
+        output = self.fp_func(
+            input,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding)
+        self.check_or_setup_grad_cache(output)
+        self.update_input_cache(input)
+        return output
+
+
+class BitCenterMaxPool2D(BitCenterPool2D, nn.MaxPool2d):
     def __init__(self,
                  kernel_size,
                  stride=None,
@@ -95,36 +176,25 @@ class BitCenterMaxPool2D(BitCenterLayer, nn.MaxPool2d):
             self, kernel_size=kernel_size, stride=stride, padding=padding)
         self.register_backward_hook(self.update_grad_output_cache)
 
-    def forward_lp(self, input):
-        # Need to test do_offset mode whether gradient is updated properly
-        input_lp, grad_output_lp = self.get_input_cache_grad_cache(input)
-        # input_lp = self.input_cache[self.cache_iter:(
-        #     self.cache_iter + input.size(0))].cuda()
-        # grad_output_lp = \
-        #     self.grad_output_cache[self.grad_cache_iter:(self.grad_cache_iter + input.size(0))].cuda()
-        input_delta = input
-        output = self.lp_func(input_delta, input_lp, grad_output_lp,
-                              self.kernel_size, self.stride, self.padding)
-        # self.cache_iter = (
-        #     self.cache_iter + input.size(0)) % self.n_train_sample
-        # self.grad_cache_iter = (
-        #     self.grad_cache_iter + input.size(0)) % self.n_train_sample
-        self.increment_cache_iter(input)
-        return output
 
-    def forward_fp(self, input):
-        self.check_or_setup_input_cache(input)
-        # if self.input_cache is None:
-        #     self.input_cache = self.setup_cache(input)
-        #     self.cache_iter = 0
-        output = self.fp_func(
-            input,
-            kernel_size=self.kernel_size,
-            stride=self.stride,
-            padding=self.padding)
-        self.check_or_setup_grad_cache(output)
-        # if self.grad_output_cache is None:
-        #     self.grad_output_cache = self.setup_cache(output)
-        #     self.grad_cache_iter = 0
-        self.update_input_cache(input)
-        return output
+class BitCenterAvgPool2D(BitCenterPool2D, nn.AvgPool2d):
+    def __init__(self,
+                 kernel_size,
+                 stride=None,
+                 padding=0,
+                 cast_func=void_cast_func,
+                 n_train_sample=1):
+        BitCenterLayer.__init__(
+            self,
+            fp_functional=F.avg_pool2d,
+            lp_functional=bit_center_avg_pool2d,
+            cast_func=cast_func,
+            n_train_sample=n_train_sample)
+        nn.AvgPool2d.__init__(
+            self, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.register_backward_hook(self.update_grad_output_cache)
+
+
+
+
+
